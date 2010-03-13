95383c204fd50892104f717a8670868dee089875
Greg Olszewski <gno@gropeep.org>
2010-03-12 23:22:45 -0800

    o build cmake

--- rakefile.rb
+++ rakefile.rb
@@ -1,3 +1,6 @@
 #!/usr/bin/env rake
 #system("cd orderly && cmake . && cmake && cmake test")
-system("find /usr/bin /usr/local/bin .")
+#system("find /usr/bin /usr/local/bin .")
+system("cd cmake && ./bootstrap --prefix=../cmake-out && make && make install")
+system("cd orderly && ../cmake-out/bin/cmake . && make && make test")
+
